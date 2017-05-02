6e3dd84b7362fcc346fb9bdde6a8cbd201547c7c
HudaSyed <who.the.syed@gmail.com>
2017-05-02 01:33:17 -0700

    Scaffolded Models, Controllers and Added Folders for Login Pages
    Added base classes for User/Game Controller as well as Models. Included
    folders for Login, NewAccount and ChangePassword in the Apps folder.

---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSON_Student_App.Models
+{
+    public class Users
+    {
+    }
+}
