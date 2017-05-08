11b742031e95d48b5ee14b30473716de393a461b
HudaSyed <who.the.syed@gmail.com>
2017-05-08 14:04:33 -0700

    Semi commenting login

--- ClientApp/app/components/login/login.component.ts
+++ ClientApp/app/components/login/login.component.ts
@@ -11,30 +11,31 @@ export class LoginComponent implements OnInit {
     //loading = false;
     //returnUrl: string;
 
-    //constructor(
-    //    private route: ActivatedRoute,
-    //    private router: Router,
-    //    private authenticationService: AuthenticationService,
-    //    ) { }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+    ) { }
 
-    //ngOnInit() {
-    //    // reset login status
-    //    this.authenticationService.logout();
+    ngOnInit() {
+        //reset login status
+        this.authenticationService.logout();
 
-    //    // get return url from route parameters or default to '/'
-    //    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    //}
+        //    // get return url from route parameters or default to '/'
+        //    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+        //}
 
-    //login() {
-    //    this.loading = true;
-    //    this.authenticationService.login(this.model.username, this.model.password)
-    //        .subscribe(
-    //            data => {
-    //                this.router.navigate([this.returnUrl]);
-    //            },
-    //            error => {
-    //                //.alertService.error(error);
-    //                this.loading = false;
-    //            });
-    //}
+        //login() {
+        //    this.loading = true;
+        //    this.authenticationService.login(this.model.username, this.model.password)
+        //        .subscribe(
+        //            data => {
+        //                this.router.navigate([this.returnUrl]);
+        //            },
+        //            error => {
+        //                //.alertService.error(error);
+        //                this.loading = false;
+        //            });
+        //}
+    }
 }
