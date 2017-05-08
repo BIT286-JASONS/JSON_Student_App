b46e92f69f1e8697b3231726eba8731b84ecaadd
lucas <kirby317@msn.com>
2017-05-08 12:59:41 -0700

    login component

--- ClientApp/app/components/login/login.component.ts
+++ ClientApp/app/components/login/login.component.ts
@@ -1,10 +1,41 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html'
+    moduleId: module.id,
+    templateUrl: 'login.component.html'
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    returnUrl: string;
 
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        ) { }
+
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    //.alertService.error(error);
+                    this.loading = false;
+                });
+    }
 }
