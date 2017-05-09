72e554bfc2132dc1bb25a1de160403f008e53648
HudaSyed <who.the.syed@gmail.com>
2017-05-09 05:01:02 -0700

    bringing back login

--- ClientApp/app/components/login/login.component.ts
+++ ClientApp/app/components/login/login.component.ts
@@ -1,41 +1,12 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AuthenticationService } from '../../services/authentication.service';
+﻿import { Component } from '@angular/core';
 
 @Component({
     templateUrl: 'login.component.html'
+    selector: 'login',
+    templateUrl: './login.component.html'
 })
+export class LoginComponent {
 
-export class LoginComponent implements OnInit {
-    //model: any = {};
-    //loading = false;
-    //returnUrl: string;
 
-    constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private authenticationService: AuthenticationService,
-    ) { }
-
-    ngOnInit() {
-        //reset login status
-        this.authenticationService.logout();
-
-        //    // get return url from route parameters or default to '/'
-        //    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-        //}
-
-        //login() {
-        //    this.loading = true;
-        //    this.authenticationService.login(this.model.username, this.model.password)
-        //        .subscribe(
-        //            data => {
-        //                this.router.navigate([this.returnUrl]);
-        //            },
-        //            error => {
-        //                //.alertService.error(error);
-        //                this.loading = false;
-        //            });
-        //}
-    }
 }
+
