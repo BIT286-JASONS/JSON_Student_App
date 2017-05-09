e95b56755b7bffda9d9e3cc863dc052cb137a7ac
lucas <kirby317@msn.com>
2017-05-09 00:54:11 -0700

    getting somewhere on login

--- ClientApp/app/components/login/login.component.ts
+++ ClientApp/app/components/login/login.component.ts
@@ -1,10 +1,31 @@
 ﻿import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { user } from './user'
+
 
 @Component({
     selector: 'login',
     templateUrl: './login.component.html'
 })
 export class LoginComponent {
+    constructor(
+        public username: string,
+        public password: string
+
+    ) { }
+
+
+
+    onSubmit() {
+
+    }
+
+
+    public login()
+    {
+
+    }
 
 
 }
---
+++ ClientApp/app/components/login/user.ts
@@ -0,0 +1,7 @@
+﻿export class user {
+    constructor(
+        
+        public username: string,
+        public password: string
+    ) { }
+}
