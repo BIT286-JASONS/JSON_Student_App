f392e4d2042909fa9c91a1bb98dd10cf8b85491f
HudaSyed <who.the.syed@gmail.com>
2017-05-09 04:57:46 -0700

    adding services

--- ClientApp/app/services/teacher/creation.service.ts
+++ ClientApp/app/services/teacher/creation.service.ts
@@ -6,7 +6,7 @@ import { Observable } from "rxjs/Rx";
 export class CreationService {
 
     constructor(private http: Http) { }
-
+    
     getTeacher() {
         return this.http.get('/api/teacher/Get').map((res: Response) => res.json());
     }
@@ -17,6 +17,7 @@ export class CreationService {
         let body = JSON.stringify(teacher);
         return this.http.post('/api/teacher/create', body, headers).map((res: Response) => res.json());
     }
+    
     //updateFood(food) {
     //    let headers = new Headers({ 'Content-Type': 'application/json' });
     //    let options = new RequestOptions({ headers: headers });
