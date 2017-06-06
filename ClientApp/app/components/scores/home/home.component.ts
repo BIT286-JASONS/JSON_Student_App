caa39d0ef219bb6509a8a540697310fd72976506
christopher Lafferty <chrisjlafferty@gmail.com>
2017-06-06 15:17:50 -0700

    index_page and home page css and html stuff

--- ClientApp/app/components/scores/home/home.component.ts
+++ ClientApp/app/components/scores/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
+   
 })
 export class HomeComponent {
 }
